0388aa56836b8997add4ea6b272a8226787b14ed
Lautaro Zanzot Lescano <33968558+LautaroZanzot@users.noreply.github.com>
2024-05-30 12:05:40 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://lautarozanzot.github.io/ContenedoresMG.github.io/",
+  base: "/ContenedoresMG.github.io/",
   plugins: [react()],
 })
