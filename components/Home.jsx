820d6ed28853e5c9762893025f8b84a66ea35782
Lautaro Zanzot Lescano <lautarozanzotlescano@gmail.com>
2024-05-30 14:49:47 -0300

    Descripción de los cambios

--- components/Home.jsx
+++ components/Home.jsx
@@ -19,15 +19,13 @@ function Home() {
           className="absolute inset-x-0 mt-32 flex justify-center items-center"
         >
           <div className="max-w-md md:mx-auto mt-16 mx-8 md:ml-72 rounded-lg z-20" style={{ backgroundColor: 'rgba(255, 255, 255, 0.75)', padding: '20px' }}>
-            <h2 className="text-3xl mx-4 md:text-5xl text-black font-bold leading-tight">
-              Mantenimiento a Contenedores Reefers y Carga Seca
-            </h2>
-            <p className="md:text-xl mx-4 text-grey font-semibold text-left">
-              Bienvenido a Contenedores MG, tu socio de confianza para el mantenimiento de contenedores refrigerados.
-              Nos enorgullecemos de brindar un servicio de clase mundial que garantiza un rendimiento óptimo  de su equipo de refrigeración.
-              Nuestro equipo altamente calificado tiene años de experiencia en la industria y está comprometido en mantener
-              su cadena de frío intacta y sus operaciones funcionando sin problemas con soluciones personalizadas y eficientes.
-              Vea cómo puede proteger sus productos y optimizar sus procesos.
+          <h2 className="text-4xl md:text-5xl text-black font-bold leading-tight mx-4">
+            Service a Contenedores Reefers
+          </h2>
+            <p className="md:text-3xl mx-4 text-grey font-semibold text-left">
+            En ContenedoresMG, sabemos lo importante que es mantener tus productos a la temperatura adecuada. 
+            Nos especializamos en el mantenimiento y reparación de contenedores refrigerados (reefers). 
+            Nuestro equipo de expertos se asegura de que tus contenedores funcionen perfectamente, protegiendo lo que más valoras. 
             </p>
           </div>
         </motion.div>
